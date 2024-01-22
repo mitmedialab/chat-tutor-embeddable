08616129a28525b79da9a31d89e1c0a5978c1bb7
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2024-01-22 18:54:23 +0000

    Attempted to delete stale branch: dummy/

--- dummy/storybook/assets/Header.stories-aojX__tB.js
+++
@@ -1,7 +0,0 @@
-import{H as m}from"./Header-t80K6f9o.js";import"./index-mK6-3EkX.js";import"./Button-iBf1bDNq.js";const g={title:"Example/Header",component:m,tags:["autodocs"],parameters:{layout:"fullscreen"}},e={args:{user:{name:"Jane Doe"}}},r={};var a,o,s;e.parameters={...e.parameters,docs:{...(a=e.parameters)==null?void 0:a.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var t,n,c;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(c=(n=r.parameters)==null?void 0:n.docs)==null?void 0:c.source}}};const i=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,i as __namedExportsOrder,g as default};
