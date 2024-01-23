41999eb0b92dd6734911801b0f15819f2cad4db2
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2024-01-23 20:59:46 +0000

    Attempted to delete stale branch: 2-keyboard-input-enter-should-fire-send-command/

--- 2-keyboard-input-enter-should-fire-send-command/storybook/assets/Message.stories-9V_BfT1C.js
+++
@@ -1,6 +0,0 @@
-import{M as r}from"./Message-CxYaZ5da.js";import"./index-Z1Yh0Bcu.js";import"./spread-rEx3vLA9.js";const l={title:"Message",component:r,tags:["autodocs"],argTypes:{content:{control:"text"},role:{control:{type:"select"},options:["assistant","user"]},timestamp:{control:"text"}},parameters:{layout:"fullscreen"}},t={args:{content:"Hello World",role:"assistant"}};var e,s,o;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    content: 'Hello World',
-    role: 'assistant'
-  }
-}`,...(o=(s=t.parameters)==null?void 0:s.docs)==null?void 0:o.source}}};const p=["base"];export{p as __namedExportsOrder,t as base,l as default};
--- 2-keyboard-input-enter-should-fire-send-command/storybook/assets/preview-d6irslGK.js
+++
@@ -1,7 +0,0 @@
-import{_ as t}from"./iframe--Noey42r.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-RvXoK0XW.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url);return new r}}};export{_ as parameters};
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-RvXoK0XW.js","./iframe--Noey42r.js","./_commonjsHelpers-5-cIlDoe.js","./index-ogXoivrg.js","./index-GNFDgxzj.js","./index-PPLHz8o0.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
