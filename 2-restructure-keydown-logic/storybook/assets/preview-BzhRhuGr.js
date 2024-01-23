537c08d697e1b9e03a9ba64f9a841b6f396b82b8
pmalacho-mit <95306112+pmalacho-mit@users.noreply.github.com>
2024-01-23 19:05:02 +0000

    Deploying to gh-pages from @ mitmedialab/chat-tutor-embeddable@0f34c7f85315cd4c423d7e8b47c64ae289b838bf 🚀

---
+++ 2-restructure-keydown-logic/storybook/assets/preview-BzhRhuGr.js
@@ -0,0 +1,7 @@
+import{_ as t}from"./iframe-aei_UrXl.js";import"../sb-preview/runtime.js";var _={docs:{renderer:async()=>{let{DocsRenderer:r}=await t(()=>import("./DocsRenderer-NNNQARDV-Ue9w1pJX.js").then(e=>e.D),__vite__mapDeps([0,1,2,3,4,5]),import.meta.url);return new r}}};export{_ as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-Ue9w1pJX.js","./iframe-aei_UrXl.js","./_commonjsHelpers-5-cIlDoe.js","./index-ogXoivrg.js","./index-GNFDgxzj.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
---
+++ 2-restructure-keydown-logic/types.d.ts
@@ -0,0 +1,28 @@
+export type Message = {
+    sender: 'Student' | 'Assistant';
+    timestamp: string;
+    role: 'user' | 'assistant';
+    content: string;
+    delay?: Number;
+    valid_docs?: any[];
+    chat_k?: string;
+    clear_number?: string;
+    time_created?: string;
+};
+export type ChatTutor = {
+    conversation: Message[];
+    selectedModel: "gpt-3.5-turbo-16k" | "gpt-3.5-turbo-8k";
+    multiple: boolean;
+    collection?: string[];
+    from_doc?: string;
+    credential_token?: string;
+};
+export type Configuration = {
+    collections: string[];
+    askEndpoint: string;
+    addEndpoint: string;
+    model: ChatTutor["selectedModel"];
+};
+export type DataMessage = {
+    message: Message;
+};
