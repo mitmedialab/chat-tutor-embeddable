418d7f632695e63b21d3d7294a8a26e6cb04179d
shengh318 <103220922+shengh318@users.noreply.github.com>
2024-01-26 16:20:48 +0000

    Deploying to gh-pages from @ mitmedialab/chat-tutor-embeddable@97c976a0047a6d2bc663e3e8549c2e2878d3697d 🚀

--- 3-internal-messages-array-should-be-implemented-as-svelte-store/storybook/assets/Window.stories-MBgdIFcv.js
+++
@@ -1,6 +0,0 @@
-import{W as s}from"./Window-FCju5enI.js";import{d as r}from"./utils-EZVoP3fW.js";import"./index-Rea00llB.js";import"./Input-w0rmq62v.js";import"./messageStore-n1Ty7YHs.js";import"./MessageContainer-FbKDjlax.js";import"./Message-OkQi32Fa.js";import"./spread-rEx3vLA9.js";const f={title:"Window",component:s,tags:["autodocs"],argTypes:{},parameters:{layout:"fullscreen"}},o={args:{initialMessage:"Welcome to ChatTutor, feel free to ask any questions about this lesson.",configuration:r}};var t,e,a;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
-  args: {
-    initialMessage: "Welcome to ChatTutor, feel free to ask any questions about this lesson.",
-    configuration: dummyConfiguration
-  }
-}`,...(a=(e=o.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const g=["base"];export{g as __namedExportsOrder,o as base,f as default};
