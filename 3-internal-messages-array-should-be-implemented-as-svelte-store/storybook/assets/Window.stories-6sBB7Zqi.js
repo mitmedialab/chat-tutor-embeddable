9a4a079ad9753c70d4d9d1f954bafea472d4cd17
Github Action (authored by pmalacho-mit) <pmalacho-mit@users.noreply.github.com>
2024-01-29 23:33:04 +0000

    Attempted to delete stale branch: 3-internal-messages-array-should-be-implemented-as-svelte-store/

--- 3-internal-messages-array-should-be-implemented-as-svelte-store/storybook/assets/Window.stories-6sBB7Zqi.js
+++
@@ -1,6 +0,0 @@
-import{W as e}from"./Window-FCju5enI.js";import{d as s}from"./utils-EZVoP3fW.js";import"./index-Rea00llB.js";import"./Input-w0rmq62v.js";import"./messageStore-n1Ty7YHs.js";import"./MessageContainer-FbKDjlax.js";import"./Message-OkQi32Fa.js";import"./spread-rEx3vLA9.js";const g={title:"Window",component:e,tags:["autodocs"],argTypes:{},parameters:{layout:"fullscreen"}},o={args:{configuration:s}};var r,t,a;o.parameters={...o.parameters,docs:{...(r=o.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  args: {
-    // initialMessage: "Welcome to ChatTutor, feel free to ask any questions about this lesson.",
-    configuration: dummyConfiguration
-  }
-}`,...(a=(t=o.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};const l=["base"];export{l as __namedExportsOrder,o as base,g as default};
